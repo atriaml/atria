ae9b7621855cc06902849a9c3c42c2bf3914a508
saifullah3396 <saifullah3396@gmail.com>
2025-12-19 17:31:33 +0100

    refactor: Update model pipeline function parameters and reorganize imports

--- atria_models/src/atria_models/api/models.py
+++ atria_models/src/atria_models/api/models.py
@@ -3,10 +3,10 @@
 from __future__ import annotations
 
 from atria_logger import get_logger
-from atria_types._datasets import DatasetLabels
-
 from atria_models.core.model_pipelines._model_pipeline import ModelPipeline
 from atria_models.registry import MODEL_PIPELINE
+from atria_models.registry.registry_groups import MODEL
+from atria_types._datasets import DatasetLabels
 
 logger = get_logger(__name__)
 
@@ -19,7 +19,17 @@ def load_model_pipeline_config(model_name: str, **kwargs):
 
 
 def load_model_pipeline(
-    model_name: str, labels: DatasetLabels, **kwargs
+    model_pipeline_name: str, labels: DatasetLabels, **kwargs
 ) -> ModelPipeline:
-    config = load_model_pipeline_config(model_name, **kwargs)
+    config = load_model_pipeline_config(model_pipeline_name, **kwargs)
     return config.build(labels=labels)
+
+
+def load_model_config(model_name: str, **kwargs):
+    logger.debug(f"Loading model config for model: {model_name} with params: {kwargs}")
+    return MODEL.load_module_config(model_name, **kwargs)
+
+
+def load_model(model_name: str, **kwargs):
+    config = load_model_config(model_name, **kwargs)
+    return config.build()
