58890debf4218dbe66d788298276def30d9bce75
saifullah3396 <saifullah3396@gmail.com>
2025-12-19 17:31:50 +0100

    refactor: Update imports in build_registry.py to specify model pipeline modules

--- atria_models/src/atria_models/build_registry.py
+++ atria_models/src/atria_models/build_registry.py
@@ -1,4 +1,6 @@
-from atria_models import *  # noqa
+from atria_models.core.model_pipelines._image_pipeline import *  # noqa
+from atria_models.core.model_pipelines._model_pipeline import *  # noqa
+from atria_models.core.model_pipelines._sequence_pipeline import *  # noqa
 from atria_models import MODEL_PIPELINE
 
 if __name__ == "__main__":
