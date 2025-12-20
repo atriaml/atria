1eed82276de67d0874edee0829dc5c9216d23fbb
saifullah3396 <saifullah3396@gmail.com>
2025-12-20 19:36:52 +0100

    refactor: Update METRICS dump to include refresh option and reorganize registry structure

--- atria_metrics/src/atria_metrics/build_registry.py
+++ atria_metrics/src/atria_metrics/build_registry.py
@@ -5,4 +5,4 @@ from atria_metrics.registry.question_answering import *  # noqa
 from atria_metrics.core._registry_group import METRICS
 
 if __name__ == "__main__":
-    METRICS.dump()
+    METRICS.dump(refresh=True)
