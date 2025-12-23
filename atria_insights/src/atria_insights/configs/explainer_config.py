daec055903b38f76ca24397a6e82c5977e2b0cae
saifullah3396 <saifullah3396@gmail.com>
2025-12-23 19:41:44 +0100

    Refactor ExplanationTaskConfig to streamline imports and enhance parameter handling in from_training_task_config method

--- atria_insights/src/atria_insights/configs/explainer_config.py
+++ atria_insights/src/atria_insights/configs/explainer_config.py
@@ -8,27 +8,24 @@ from atria_ml.configs import (
     TaskConfigBase,
     TrainingTaskConfig,
 )
+from atria_ml.training._configs import LoggingConfig
 from pydantic import ConfigDict
 
+from atria_insights.baseline_generators import BaselineGeneratorConfigType
+from atria_insights.baseline_generators._simple import SimpleBaselineGeneratorConfig
 from atria_insights.explainability_metrics._base import ExplainabilityMetricConfig
 from atria_insights.explainers._torchxai import (
     ExplainerConfigType,
     SaliencyExplainerConfig,
 )
+from atria_insights.feature_segmentors import (
+    FeatureSegmentorConfigType,
+    NoOpSegmenterConfig,
+)
 from atria_insights.model_pipelines._api import load_x_model_pipeline_config
 from atria_insights.model_pipelines._common import (
     ExplainableModelPipelineConfig,
     ExplanationTargetStrategy,
-)
-from atria_insights.model_pipelines._feature_segmentors import (
-    FeatureSegmentorConfigType,
-    NoOpSegmenterConfig,
-)
-from atria_insights.model_pipelines.baseline_generators import (
-    BaselineGeneratorConfigType,
-)
-from atria_insights.model_pipelines.baseline_generators._simple import (
-    SimpleBaselineGeneratorConfig,
 )
 
 logger = get_logger(__name__)
@@ -40,14 +37,16 @@ class ExplanationTaskConfig(TaskConfigBase):
     )
     x_model_pipeline: ExplainableModelPipelineConfig
     enable_outputs_caching: bool = False
+    max_training_baseline_features: int = 100
 
     @classmethod
     def from_training_task_config(
         cls,
-        config: TrainingTaskConfig,
+        training_task_config: TrainingTaskConfig,
         dataset_name: str | None = None,
         exp_name: str = "img_cls_00",
         output_dir: str = "./outputs",
+        logging: LoggingConfig = LoggingConfig(refresh_rate=1, logging_steps=1),
         feature_segmentor: FeatureSegmentorConfigType = NoOpSegmenterConfig(),
         baseline_generator: BaselineGeneratorConfigType = SimpleBaselineGeneratorConfig(),
         explainer: ExplainerConfigType = SaliencyExplainerConfig(),
@@ -57,18 +56,21 @@ class ExplanationTaskConfig(TaskConfigBase):
         ),
         iterative_computation: bool = False,
         enable_outputs_caching: bool = False,
+        max_training_baseline_features: int = 100,
+        internal_batch_size: int = 1,
+        grad_batch_size: int = 1,
     ) -> ExplanationTaskConfig:
         if dataset_name is not None:
             data = DataConfig(
                 dataset_config=load_dataset_config(dataset_name), num_workers=8
             )
-            if data != config.data:
+            if data != training_task_config.data:
                 logger.warning(
                     "The provided dataset_name results in a different DataConfig than the one in the TrainingRunConfig. Using the new DataConfig."
                 )
         else:
-            data = config.data
-        assert config.env.model_name is not None, (
+            data = training_task_config.data
+        assert training_task_config.env.model_name is not None, (
             "Model name must be specified in the TrainingRunConfig."
         )
         return ExplanationTaskConfig(
@@ -76,20 +78,24 @@ class ExplanationTaskConfig(TaskConfigBase):
                 project_name="docinsights",
                 exp_name=exp_name,
                 dataset_name=data.dataset_config.dataset_name.replace("/", "_"),
-                model_name=config.env.model_name.replace("/", "_"),
+                model_name=training_task_config.env.model_name.replace("/", "_"),
                 output_dir=output_dir,
-                seed=config.env.seed,
+                seed=training_task_config.env.seed,
             ),
             data=data,
             x_model_pipeline=load_x_model_pipeline_config(
-                config.model_pipeline.name,
-                model_pipeline=config.model_pipeline,
+                training_task_config.model_pipeline.name,
+                model_pipeline=training_task_config.model_pipeline,
                 feature_segmentor=feature_segmentor,
                 baseline_generator=baseline_generator,
                 explainer=explainer,
                 explainability_metrics=explainability_metrics,
                 explanation_target_strategy=explanation_target_strategy,
                 iterative_computation=iterative_computation,
+                internal_batch_size=internal_batch_size,
+                grad_batch_size=grad_batch_size,
             ),
             enable_outputs_caching=enable_outputs_caching,
+            max_training_baseline_features=max_training_baseline_features,
+            logging=logging,
         )
