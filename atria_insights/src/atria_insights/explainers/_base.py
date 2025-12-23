e05102a7cb245fac1477e57f7af5adb2ffcc11a0
saifullah3396 <saifullah3396@gmail.com>
2025-12-23 19:40:03 +0100

    Refactor ExplainerConfig build method to include internal_batch_size parameter; remove grad_batch_size handling.

--- atria_insights/src/atria_insights/explainers/_base.py
+++ atria_insights/src/atria_insights/explainers/_base.py
@@ -12,17 +12,25 @@ if TYPE_CHECKING:
 
 class ExplainerConfig(ModuleConfig):
     __builds_with_kwargs__: bool = True
-    internal_batch_size: int = 64
-    grad_batch_size: int = 64
 
     @property
     def kwargs(self) -> dict[str, Any]:
         return self.model_dump(exclude={"module_path", "type"})
 
     def build(  # type: ignore
-        self, model: torch.nn.Module, multi_target: bool = False, **kwargs
+        self,
+        model: torch.nn.Module,
+        internal_batch_size: int = 1,
+        multi_target: bool = False,
+        **kwargs,
     ) -> Explainer:
-        return super().build(model=model, multi_target=multi_target, **kwargs)
+        kwargs.pop("grad_batch_size")
+        return super().build(
+            model=model,
+            internal_batch_size=internal_batch_size,
+            multi_target=multi_target,
+            **kwargs,
+        )
 
 
 T_ExplainerConfig = TypeVar("T_ExplainerConfig", bound=ExplainerConfig)
