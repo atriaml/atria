5ec07eab9300e718d92b1123f8db4c7ebffa9e5e
saifullah3396 <saifullah3396@gmail.com>
2025-12-23 02:48:00 +0100

    Update exampls.

---
+++ examples/explain_cifar10/explain_cifar10.py
@@ -0,0 +1,30 @@
+import fire
+from atria_datasets.registry.image_classification.cifar10 import Cifar10  # noqa: F401
+
+from atria_insights import ExplanationTaskConfig, ModelExplainer
+
+
+def main(
+    checkpoint_path: str,
+    dataset_name: str | None = None,
+    exp_name: str = "explain_img_cls_00",
+    output_dir: str = "./outputs",
+):
+    import torch
+    from atria_ml.configs import TrainingTaskConfig
+
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    config = TrainingTaskConfig.from_dict(checkpoint["config"])
+    config = ExplanationTaskConfig.from_training_task_config(
+        config=config,
+        dataset_name=dataset_name,
+        exp_name=exp_name,
+        output_dir=output_dir,
+    )
+    model_explainer = ModelExplainer(config=config)
+    state = model_explainer.run(checkpoint_path=checkpoint_path, total_samples=10)
+    print(state.output)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
--- examples/train_cifar10/train_cifar10.py
+++ examples/train_cifar10/train_cifar10.py
@@ -1,39 +1,33 @@
 from atria_datasets.api.datasets import load_dataset_config
 from atria_datasets.registry.image_classification.cifar10 import Cifar10  # noqa: F401
-from atria_ml.task_pipelines._trainer import Trainer
-from atria_ml.task_pipelines.configs._base import (
+from atria_ml.configs import (
     DataConfig,
-    RunConfig,
     RuntimeEnvConfig,
     TrainerConfig,
+    TrainingTaskConfig,
 )
+from atria_ml.task_pipelines._trainer import Trainer
 from atria_models.api.models import load_model_pipeline_config
 from atria_models.core.model_pipelines._common import ModelConfig
 from atria_transforms.api.tfs import load_transform
 
-config = RunConfig(
+config = TrainingTaskConfig(
     env=RuntimeEnvConfig(
         project_name="my_atria_project",
-        run_name="cifar10_experiment_03",
+        exp_name="train",
+        dataset_name="cifar10",
+        model_name="resnet50",
         output_dir="./outputs/",
         seed=42,
     ),
     model_pipeline=load_model_pipeline_config(
         "image_classification",
         model=ModelConfig(model_name_or_path="resnet50"),
-        train_transform=load_transform(
-            "image_processor",
-            tf={
-                "use_imagenet_mean_std": True,
-            },
-        ),
-        eval_transform=load_transform(
-            "image_processor", tf={"use_imagenet_mean_std": True}
-        ),
+        train_transform=load_transform("image_processor", tf={"stats": "imagenet"}),
+        eval_transform=load_transform("image_processor", tf={"stats": "imagenet"}),
     ),
     data=DataConfig(
         dataset_config=load_dataset_config("cifar10/default"),
-        data_dir="data_dir/",
         num_workers=0,
         train_batch_size=64,
         eval_batch_size=64,
@@ -41,15 +35,5 @@ config = RunConfig(
     trainer=TrainerConfig(),
     do_train=True,
 )
-# exit()
-# config_dict_before = config.model_dump()
-# config.save_to_json()
-# config.from_json(Path(config.env.output_dir) / "config.json")
-# config_dict_after = config.model_dump()
-# if config_dict_before != config_dict_after:
-#     raise ValueError("Config serialization/deserialization failed!")
-# else:
-#     print("Config serialization/deserialization successful!")
-
 trainer = Trainer(config=config)
 trainer.run()
