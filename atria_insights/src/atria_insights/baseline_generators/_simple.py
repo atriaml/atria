ec558132b4526decb588716014ee44116877991f
saifullah3396 <saifullah3396@gmail.com>
2025-12-26 16:51:43 +0100

    Refactor baseline preparation logic into a method within SimpleBaselineGenerator

--- atria_insights/src/atria_insights/baseline_generators/_simple.py
+++ atria_insights/src/atria_insights/baseline_generators/_simple.py
@@ -22,22 +22,41 @@ class SimpleBaselineGeneratorConfig(ModuleConfig):
 class SimpleBaselineGenerator(BaselineGenerator[SimpleBaselineGeneratorConfig]):
     __config__ = SimpleBaselineGeneratorConfig
 
+    def _prepare_baselines_from_type(
+        self,
+        inp: torch.Tensor,
+        baselines_type: Literal["zeros", "ones", "batch_mean", "random", "fixed"],
+        fixed_value: float = 0.5,
+    ) -> torch.Tensor:
+        if baselines_type == "zeros":
+            return torch.zeros_like(inp)
+        elif baselines_type == "ones":
+            return torch.ones_like(inp)
+        elif baselines_type == "batch_mean":
+            return torch.full_like(inp, inp.mean().item())
+        elif baselines_type == "fixed":
+            return torch.full_like(inp, fixed_value)
+        elif baselines_type == "random":
+            return torch.rand_like(inp)
+        else:
+            raise ValueError(
+                f"Unsupported baselines_type: {baselines_type}. Supported types are 'zeros' and 'random'."
+            )
+
     def __call__(  # type: ignore[override]
         self, inputs: torch.Tensor | OrderedDict[str, torch.Tensor]
     ) -> torch.Tensor | OrderedDict[str, torch.Tensor]:
-        from torchxai.ignite._utilities import _prepare_baselines_from_type
-
         if isinstance(inputs, OrderedDict):
             baselines = OrderedDict()
             for key, tensor in inputs.items():
-                baselines[key] = _prepare_baselines_from_type(
+                baselines[key] = self._prepare_baselines_from_type(
                     tensor,
                     baselines_type=self.config.baseline_strategy,  # type: ignore[arg-type]
                     fixed_value=self.config.baselines_fixed_value,
                 )
             return baselines
         else:
-            return _prepare_baselines_from_type(
+            return self._prepare_baselines_from_type(
                 inputs,
                 baselines_type=self.config.baseline_strategy,  # type: ignore[arg-type]
                 fixed_value=self.config.baselines_fixed_value,
