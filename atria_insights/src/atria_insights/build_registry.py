0dd354bcfc89fa968f9c639d1667f3b5df5d771a
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2026-04-02 12:43:46 +0200

    Refactor code structure for improved readability and maintainability

--- atria_insights/src/atria_insights/build_registry.py
+++ atria_insights/src/atria_insights/build_registry.py
@@ -1,22 +1,18 @@
 from atria_insights.explainability_metrics._registry_group import EXPLAINABILITY_METRICS
 from atria_insights.explainers._registry_group import EXPLAINERS
+from atria_insights.model_pipelines._attn_sequence_pipeline import *  # noqa
 from atria_insights.model_pipelines._image_pipeline import *  # noqa
 from atria_insights.model_pipelines._registry_groups import (
     EXPLAINABLE_MODEL_PIPELINES,
     # noqa
 )
-from atria_insights.perturbation_robustness._registry_groups import (
-    PERTURBATION_ROBUSTNESS_PIPELINES,  # noqa
-)
-from atria_insights.perturbation_robustness.pipelines._image_pipeline import *  # noqa
-from atria_insights.perturbation_robustness.pipelines._sequence_pipeline import *  # noqa
+from atria_insights.model_pipelines._sequence_pipeline import *  # noqa
 
 
 def main():
     EXPLAINABLE_MODEL_PIPELINES.dump(refresh=True)
     EXPLAINERS.dump()
     EXPLAINABILITY_METRICS.dump()
-    PERTURBATION_ROBUSTNESS_PIPELINES.dump()
 
 
 if __name__ == "__main__":
