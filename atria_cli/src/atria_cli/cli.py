03a8850b84a8300fe7fcfd8c95126da9cad02903
saifullah3396 <saifullah3396@gmail.com>
2026-05-28 15:46:24 +0200

    remove leftover code. update cli.

--- atria_cli/src/atria_cli/cli.py
+++ atria_cli/src/atria_cli/cli.py
@@ -1,7 +1,7 @@
 import fire
 from atria_logger import get_logger
 
-from atria_cli.commands import models
+from atria_cli.commands import datasets
 from atria_cli.commands.sign_in import sign_in
 from atria_cli.commands.sign_out import sign_out
 from atria_cli.commands.sign_up import sign_up
@@ -15,10 +15,10 @@ if __name__ == "__main__":
             "sign_in": sign_in,
             "sign_out": sign_out,
             "sign_up": sign_up,
-            # "datasets": {
-            #     "prepare_and_upload": datasets.prepare_and_upload,
-            #     "download": datasets.download,
-            # },
-            "models": {"upload": models.upload, "download": models.download},
+            "datasets": {
+                "prepare_and_upload": datasets.prepare_and_upload,
+                "download": datasets.download,
+            },
+            # "models": {"upload": models.upload, "download": models.download},
         }
     )
