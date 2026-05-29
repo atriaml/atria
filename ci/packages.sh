503ec9c720cd89595d6b234bf90015815ec8e2ba
saifullah3396 <saifullah3396@gmail.com>
2026-05-29 11:01:00 +0200

    Updatep packages

--- ci/packages.sh
+++ ci/packages.sh
@@ -4,4 +4,11 @@ declare -a packages=(
     atria_logger
     atria_types
     atria_registry
+    atria_datasets
+    atria_cli
+    atria_hub
+    atria_metrics
+    atria_models
+    atria_ml
+    atria_insights
 )
