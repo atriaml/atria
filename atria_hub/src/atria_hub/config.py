627eec47cdebf1356c9eea4200381448c1f0d1b0
saifullah3396 <saifullah3396@gmail.com>
2026-05-29 10:14:39 +0200

    refactor.

--- atria_hub/src/atria_hub/config.py
+++ atria_hub/src/atria_hub/config.py
@@ -7,8 +7,6 @@ class Settings(BaseSettings):
     ATRIAX_URL: str = "http://127.0.0.1:8000"
     ATRIAX_STORAGE_URL: str = "http://127.0.0.1:8090"
     ATRIAX_ANON_KEY: str = "sb_publishable_cY44rYmTawJwU1coFeruPX__momBm12"
-    LOG_FORMAT: str = "%(message)s"
-    DATE_FORMAT: str = "[%X]"
 
 
 settings = Settings()  # type: ignore
